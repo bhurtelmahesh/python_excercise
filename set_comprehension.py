f344b9bfd5352fd30999bdfdf6d20d00a2db7ed3
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-26 04:00:15 +0900

    set comprehension

--- set_comprehension.py
+++ set_comprehension.py
@@ -27,12 +27,12 @@ print(dict4) #{1: 2, 2: 4, 3: 6, 4: 8}
 
 # pair a num with its double as a dict
 
-
-
+#pair all the alphabets with their respective numbers
 z = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 dict5 = {k:v for k,v in zip(z, range(1,100))}
 print(dict5) #{'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
 
+#find the duplicates using list comprehension and set comprehension
 some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
-duplicates = [i for i in some_list if some_list.count(i)>1 and i not in duplicates]
+duplicates =list(set([i for i in some_list if some_list.count(i)>1]))
 print(duplicates) #['b', 'n']

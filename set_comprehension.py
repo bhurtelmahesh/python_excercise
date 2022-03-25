193804c656107fd85c851f59b998f255cad68fb7
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-26 04:02:03 +0900

    set comprehension

--- set_comprehension.py
+++ set_comprehension.py
@@ -34,5 +34,6 @@ print(dict5) #{'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, '
 
 #find the duplicates using list comprehension and set comprehension
 some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
-duplicates =list(set([i for i in some_list if some_list.count(i)>1]))
+# duplicates =list(set([i for i in some_list if some_list.count(i)>1]))
+duplicates = list({i for i in some_list if some_list.count(i)>1})
 print(duplicates) #['b', 'n']
